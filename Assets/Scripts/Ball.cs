6c4f55054bd080c31e21cf2ecf2d1089362365d8
KodeAndGame <alvarez.richard@gmail.com>
2013-11-03 14:08:35 -0500

    moved ball reset function to a more logical place

---
+++ Assets/Scripts/Ball.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Ball : MonoBehaviour {
+	void OnBecameInvisible () {
+		Reset ();
+	}
+	void Reset () {
+		if(gameObject.active) {
+			rigidbody.isKinematic = false;
+			rigidbody.detectCollisions = true;
+			transform.position = Vector3.zero;
+			rigidbody.velocity = Vector3.zero;
+			if(transform.parent != null && transform.parent.gameObject.active && transform.parent.parent.gameObject.active) {
+				transform.parent = null;
+			}
+		}
+	}
+}
