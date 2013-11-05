adc98d983491e8d48e0410747225d316b76e67c4
KodeAndGame <alvarez.richard@gmail.com>
2013-11-05 09:14:36 -0500

    removed obsolte GameObject.active reference (no longer necessary anyway)

--- Assets/Scripts/Ball.cs
+++ Assets/Scripts/Ball.cs
@@ -10,14 +10,10 @@ public class Ball : MonoBehaviour {
 		}
 	}
 	void Reset () {
-		if(gameObject.active) {
-			rigidbody.isKinematic = false;
-			rigidbody.detectCollisions = true;
-			transform.position = Vector3.zero;
-			rigidbody.velocity = Vector3.zero;
-			if(transform.parent != null && transform.parent.gameObject.active && transform.parent.parent.gameObject.active) {
-				transform.parent = null;
-			}
-		}
+		rigidbody.isKinematic = false;
+		rigidbody.detectCollisions = true;
+		transform.position = Vector3.zero;
+		rigidbody.velocity = Vector3.zero;
+		transform.parent = null;
 	}
 }
