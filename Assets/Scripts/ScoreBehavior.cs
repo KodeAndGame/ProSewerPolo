9a5be97695b3daaccef02b50dcfe22f34984b549
Matt Geiger <Mail@MattGeiger.com>
2013-11-08 17:19:36 -0500

    Added Turbo Functionality
    Players can now use turbo.
    GUI not yet implemented.

--- Assets/Scripts/ScoreBehavior.cs
+++ Assets/Scripts/ScoreBehavior.cs
@@ -5,8 +5,11 @@ public class ScoreBehavior : MonoBehaviour {
 	
 	public GUIStyle scoreStyle;
 	public int redScore = 0, blueScore = 0;
+	public SwimmerBehavior Swimmer1;
 	
 	void OnGUI(){
+		
+		GUI.Label(new Rect(10, 70, 100, 100), Swimmer1.TurboAmount.ToString(), scoreStyle);//Turbo
 		
 		GUI.Label(new Rect(10, 10, 100, 100), blueScore.ToString(), scoreStyle);//blueScore
 		GUI.Label(new Rect(Screen.width - 45, 10, 100, 100), redScore.ToString(), scoreStyle);//redScore
