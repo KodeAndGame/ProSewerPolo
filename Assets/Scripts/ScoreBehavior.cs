83c7e94cf0b3df0594795b5f9a5fdb961ef9c7ab
Matt Geiger <Mail@MattGeiger.com>
2013-11-09 09:47:45 -0500

    Completed Turbo GUI
    I finished the GUI for both team's tubo bar.
    The button mappings still need to be set for game pad.

--- Assets/Scripts/ScoreBehavior.cs
+++ Assets/Scripts/ScoreBehavior.cs
@@ -3,16 +3,26 @@ using System.Collections;
 
 public class ScoreBehavior : MonoBehaviour {
 	
-	public GUIStyle scoreStyle;
+	public GUIStyle BlueScoreStyle;
+	public GUIStyle RedScoreStyle;
+	public GUIStyle BlueTurboStyle;
+	public GUIStyle RedTurboStyle;
 	public int redScore = 0, blueScore = 0;
 	public SwimmerBehavior Swimmer1;
+	public SwimmerBehavior Swimmer2;
+	public SwimmerBehavior Swimmer3;
+	public SwimmerBehavior Swimmer4;
 	
 	void OnGUI(){
 		
-		GUI.Label(new Rect(10, 70, 100, 100), Swimmer1.TurboAmount.ToString(), scoreStyle);//Turbo
+		//Turbo Bars
+		GUI.Label(new Rect(10, 80, Swimmer1.TurboAmount, 15),"Left Turbo", BlueTurboStyle);
+		GUI.Label(new Rect(10, 100, Swimmer2.TurboAmount, 15), "Right Turbo", BlueTurboStyle);
+		GUI.Label(new Rect(Screen.width - 115, 80, Swimmer3.TurboAmount, 15), "Left Turbo ", RedTurboStyle);
+		GUI.Label(new Rect(Screen.width - 115, 100, Swimmer4.TurboAmount, 15), "Right Turbo ", RedTurboStyle);
 		
-		GUI.Label(new Rect(10, 10, 100, 100), blueScore.ToString(), scoreStyle);//blueScore
-		GUI.Label(new Rect(Screen.width - 45, 10, 100, 100), redScore.ToString(), scoreStyle);//redScore
+		GUI.Label(new Rect(10, 10, 100, 100), blueScore.ToString(), BlueScoreStyle);//blueScore
+		GUI.Label(new Rect(Screen.width - 115, 10, 100, 100), redScore.ToString(), RedScoreStyle);//redScore
 		
 	}
 }
