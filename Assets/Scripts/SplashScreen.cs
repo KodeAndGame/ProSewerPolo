d3ef11bb7dbc95fb8b72f9c093f3dfae2effa66a
Dan Dwire <doublemintm@gmail.com>
2013-11-19 18:42:39 -0500

    Added splash screen

---
+++ Assets/Scripts/SplashScreen.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+public class SplashScreen : MonoBehaviour {
+	public string[] ShootAxes;
+	public Texture splashTxt;
+	public GUIStyle SplashStyle;
+	
+	private bool showPlayText = false;
+	// Use this for initialization
+	void Start () {
+		StartCoroutine(TogglePlay());
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+		foreach (string axis in ShootAxes) {
+			if (Input.GetAxis(axis) > 0f) {
+				// go to the next level
+				Application.LoadLevel("master");
+				
+				return;	
+			}
+			
+		}
+	}
+	
+	
+	// Gui Loop
+	void OnGUI () {
+		GUI.DrawTexture(new Rect(0, 0, Camera.main.pixelWidth, Camera.main.pixelHeight), splashTxt, ScaleMode.ScaleToFit, false, 1.77f);			
+		if (showPlayText) {  	
+			GUI.Label(new Rect((Camera.main.pixelHeight/10), (Camera.main.pixelHeight/2), 400, 72), "PRESS SHOOT TO PLAY", SplashStyle);
+		}
+	}
+	
+	IEnumerator TogglePlay () {
+		showPlayText = !showPlayText;
+		yield return new WaitForSeconds(0.75f);
+		StartCoroutine(TogglePlay());
+		Debug.Log("showPlayText: " + showPlayText);
+	}
+	
+}
